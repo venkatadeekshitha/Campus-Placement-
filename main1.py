9d4296e3348b8d4c598ba7c13b27846bd234471e
venkatadeekshitha <venkatadeekshitha8@gmail.com>
2025-07-14 20:23:04 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template
 
-# Create Flask app
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def home():
-    return '<h1>Campus Placement App is Running Successfully!</h1>'
+    return '<h2>🎓 Campus Placement Predictor is running successfully on Render!</h2>'
 
-# You can add more routes if needed
-@app.route('/about')
-def about():
-    return '<p>This is the about page.</p>'
-
-# For local testing only — not needed in Render
 if __name__ == '__main__':
     app.run(debug=True)
